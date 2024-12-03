87323485857ccc4920fe95a988c299ad9d630195
Matt Noakes <matthew.noakes@sparx.co.uk>
2024-12-03 15:46:24 +0000

    Day 3 part 2

--- src/day3/index.ts
+++ src/day3/index.ts
@@ -19,12 +19,46 @@ type Operands = [number, number];
 const getOperands = (instruction: RegExpExecArray) =>
   instruction.slice(1, 3).map((operand) => parseInt(operand, 10)) as Operands;
 
+const MUL = /mul\((\d{1,3}),(\d{1,3})\)/;
+
 const solvePart1 = async () => {
-  const instructions = data.matchAll(/mul\((\d{1,3}),(\d{1,3})\)/g);
+  const matcher = new RegExp(MUL.source, "g");
+  const instructions = data.matchAll(matcher);
+
   const processed = [...instructions].map(getOperands);
 
   return processed.reduce((acc, [a, b]) => acc + a * b, 0);
 };
 
+const solvePart2 = async () => {
+  const DO_DONT = /do\(\)|don't\(\)/;
+
+  const matcher = new RegExp(`${DO_DONT.source}|${MUL.source}`, "g");
+  const instructions = data.matchAll(matcher);
+
+  let enabled = true;
+  return [...instructions].reduce<number>((acc, instruction) => {
+    switch (instruction.at(0)) {
+      case "do()":
+        enabled = true;
+        break;
+      case "don't()":
+        enabled = false;
+        break;
+      default:
+        if (enabled) {
+          const [a, b] = getOperands(instruction);
+          return acc + a * b;
+        }
+    }
+    return acc;
+  }, 0);
+};
+
+export { solvePart1, solvePart2 };
+
 const total = await solvePart1();
 console.log(`Sum of multiplications: ${total}`);
+
+const refinedTotal = await solvePart2();
+console.log(`Sum of enabled multiplications: ${refinedTotal}`);
