8ef5d47d9a80c6cd9aeb36bf17134facf98915b1
Matt Noakes <matthew.noakes@sparx.co.uk>
2024-12-05 21:34:57 +0000

    Day 5 part 2 - could be better but will return to later!

--- src/day5/index.ts
+++ src/day5/index.ts
@@ -2,7 +2,6 @@ import { resolve } from "path";
 import { fileURLToPath } from "url";
 
 import processInput from "../lib/processor";
-import range from "../lib/range";
 
 const inputPath = resolve(fileURLToPath(import.meta.url), "..", "input.txt");
 
@@ -44,35 +43,62 @@ const getData = async () => {
   };
 };
 
-const data = await getData();
+const { rules, updates } = await getData();
 
-const updateSatisfiesOrderingRule = (
-  update: number[],
-  [first, second]: OrderingRule,
-) => {
-  if (!update.includes(first) || !update.includes(second)) {
-    return true;
-  }
+const ruleApplies = (update: number[], [first, second]: OrderingRule) =>
+  update.includes(first) && update.includes(second);
 
-  return update.indexOf(first) < update.indexOf(second);
-};
+const ruleSatisfied = (update: number[], [first, second]: OrderingRule) =>
+  update.indexOf(first) < update.indexOf(second);
 
-const getMiddlePageNumber = (update: number[]) =>
-  update[(update.length - 1) / 2];
+const updateOk = (update: number[], rule: OrderingRule) =>
+  !ruleApplies(update, rule) || ruleSatisfied(update, rule);
+
+const getMiddlePage = (update: number[]) => update[(update.length - 1) / 2];
 
 const solvePart1 = async () => {
-  const { rules, updates } = data;
-  return updates.reduce((acc, update) => {
-    return (
-      acc +
-      (rules.every((rule) => updateSatisfiesOrderingRule(update, rule))
-        ? getMiddlePageNumber(update)
-        : 0)
-    );
-  }, 0);
+  const correctUpdates = updates.filter((update) =>
+    rules.every((rule) => updateOk(update, rule)),
+  );
+
+  return correctUpdates.reduce((acc, update) => acc + getMiddlePage(update), 0);
 };
 
-export { solvePart1 };
+/** Map each page in the update together with a Set of the pages that must come AFTER it. */
+const getRuleMap = (update: number[]) =>
+  rules
+    .filter((rule) => ruleApplies(update, rule))
+    .reduce(
+      (acc, [first, second]) => {
+        // Associate the second page with the first, indicating that the second must follow the first
+        acc.get(first)?.add(second);
+        return acc;
+      },
+      new Map(update.map((u) => [u, new Set<number>()])),
+    );
+
+const solvePart2 = async () => {
+  const incorrectUpdates = updates.filter((update) =>
+    rules.some((rule) => !updateOk(update, rule)),
+  );
+
+  const reOrdered = incorrectUpdates.map((update) => {
+    const relevantRules = Array.from(getRuleMap(update).entries()).filter(
+      ([first]) => update.includes(first),
+    );
+
+    return relevantRules
+      .toSorted((a, b) => b[1].size - a[1].size)
+      .map(([first]) => first);
+  });
+
+  return reOrdered.reduce((acc, x) => acc + getMiddlePage(x), 0);
+};
+
+export { solvePart1, solvePart2 };
 
 const part1Total = await solvePart1();
 console.log(`Middle pages of correct inputs sum to ${part1Total}.`);
+
+const part2Total = await solvePart2();
+console.log(`Middle pages of re-ordered inputs sum to ${part2Total}.`);
