00f41fa926eeba3a5c48a2020c741df8c98a8cab
Matt Noakes <matthew.noakes@sparx.co.uk>
2024-12-07 13:02:39 +0000

    Day 7 part 2

--- src/day7/index.ts
+++ src/day7/index.ts
@@ -19,7 +19,18 @@ const getData = async () => {
 
 const data = await getData();
 
-type Operator = "add" | "multiply";
+type Operator = "add" | "multiply" | "concatenate";
+
+const applyConcatenation = (
+  runningValue: number,
+  operand: number,
+  ops: Operator[],
+) => {
+  const newValue = parseInt(`${runningValue}${operand}`, 10);
+  return [newValue, ops.concat("concatenate")] as const;
+};
+
+let enable_concatenation = false;
 
 const findOperators = (value: number, operands: number[]) => {
   let operatorMap = new Map<number, Operator[]>([[operands[0], []]]);
@@ -31,6 +42,9 @@ const findOperators = (value: number, operands: number[]) => {
         [
           [v + operand, ops.concat("add")] as const,
           [v * operand, ops.concat("multiply")] as const,
+          ...(enable_concatenation
+            ? [applyConcatenation(v, operand, ops)]
+            : []),
         ]
           // The calculation always increases with these operators
           // So we can trim away any values already larger than the target
@@ -47,15 +61,28 @@ const findSolution = (value: number, operands: number[]) => {
   return operatorMap.get(value);
 };
 
-const solvePart1 = async () => {
-  const validLines = data
+const getSumOfSolutions = () =>
+  data
     .map((d) => ({ ...d, operators: findSolution(d.value, d.operands) }))
-    .filter((d) => d.operators !== undefined);
+    .filter((d) => d.operators !== undefined)
+    .reduce((acc, next) => acc + next.value, 0);
 
-  return validLines.reduce((acc, next) => acc + next.value, 0);
+const solvePart1 = async () => {
+  enable_concatenation = false;
+  return getSumOfSolutions();
 };
 
-export { solvePart1 };
+const solvePart2 = async () => {
+  enable_concatenation = true;
+  return getSumOfSolutions();
+};
+
+export { solvePart1, solvePart2 };
 
 const part1 = await solvePart1();
 console.log(`Total value of lines with valid solutions: ${part1}`);
+
+const part2 = await solvePart2();
+console.log(
+  `Total value of lines with valid solutions (including concatenation): ${part2}`,
+);
