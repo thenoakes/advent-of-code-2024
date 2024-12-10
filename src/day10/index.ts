d05a4139ed33df0cb3299272f453682d8033e44b
Matt Noakes <matthew.noakes@sparx.co.uk>
2024-12-10 10:45:59 +0000

    Day 10 part 1 (plus debugging!)

---
+++ src/day10/index.ts
@@ -0,0 +1,76 @@
+import { resolve } from "path";
+import { fileURLToPath } from "url";
+
+import processInput from "../lib/processor";
+import range from "../lib/range";
+
+const inputPath = resolve(fileURLToPath(import.meta.url), "..", "input.txt");
+
+const { log } = console;
+
+const getData = async () => {
+  const heights = <number[][]>[];
+  await processInput(inputPath, (line) => {
+    const digits = Array.from(line).map((char) => parseInt(char, 10));
+    heights.push(digits);
+  });
+
+  return heights;
+};
+
+const data = await getData();
+const [rowCount, colCount] = [data.length, data.at(0)!.length];
+
+const trailHeadMap = new Array<{ row: number; col: number }>();
+for (const row of range(rowCount)) {
+  for (const col of range(colCount)) {
+    if (data[row][col] === 0) {
+      trailHeadMap.push({ row, col });
+    }
+  }
+}
+
+type Coord = { row: number; col: number };
+const toKey = (coord: Coord) => `${coord.row},${coord.col}` as const;
+type CoordKey = ReturnType<typeof toKey>;
+const fromKey = (key: CoordKey): Coord => {
+  const [row, col] = key.split(",").map((x) => parseInt(x, 10));
+  return { row, col };
+};
+
+const mapTrailHead = (row: number, col: number) => {
+  let positions = new Set<CoordKey>([toKey({ row, col })]);
+  for (let i = 1; i <= 9; i++) {
+    const newPositions = new Set<CoordKey>();
+    for (const pos of positions) {
+      const { row, col } = fromKey(pos);
+      const nextPositions = [
+        { row: row - 1, col },
+        { row: row + 1, col },
+        { row, col: col - 1 },
+        { row, col: col + 1 },
+      ].filter(
+        ({ row, col }) =>
+          row >= 0 && row < rowCount && col >= 0 && col < colCount,
+      );
+
+      nextPositions.forEach((nextPos) => {
+        if (data[nextPos.row][nextPos.col] === i) {
+          newPositions.add(toKey(nextPos));
+        }
+      });
+    }
+    positions = newPositions;
+  }
+
+  // Return the number of finishing positions - this is the score for this trailhead
+  return positions.size;
+};
+
+const solvePart1 = async () => {
+  const scores = trailHeadMap.map(({ row, col }) => mapTrailHead(row, col));
+  return scores.reduce((acc, score) => acc + score, 0);
+};
+
+const part1 = await solvePart1();
+log(`Total score is ${part1}`);
